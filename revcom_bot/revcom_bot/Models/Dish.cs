7aedfcacbf8fbe812867bc3b7d03d912537673c3
Hoxtober <hoxtber@gmail.com>
2020-08-19 10:39:07 +0200

    DAL project added

--- revcom_bot/revcom_bot/Models/Dish.cs
+++ revcom_bot/revcom_bot/Models/Dish.cs
@@ -12,6 +12,7 @@ namespace revcom_bot.Models
         public string Image { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public decimal Price { get; set; }
 
        /* public string Realize(Dish dish, TelegramBotClient bot)
         {
